dba29a7c8481633402d05a966242c9c63b0d01a6
lakshminarayan7 <lakshminarayan8147@gmail.com>
2021-09-19 02:49:29 +0530

    origa_task

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+
+//Use these files
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ limit: '10mb', extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect('mongodb://localhost:27017/origa_task', { useNewUrlParser: true })
+    .then(re => console.log('Connected to origa_task DB...'))
+    .catch(err => console.log(err));
+
+// app.use('/tasks', require('./controller/taskList-controller'));
+
+app.get('/', function (req, res) {
+    res.send('invalid rest point');
+});
+
+//Listen to port 9005
+app.listen(9005, function () {
+    console.log("Server Started @ 9005:-)");
+});
+
+module.exports = app;
